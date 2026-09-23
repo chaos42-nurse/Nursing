ff0de7beb3f790b4d8fc080b53fa90b271637bb4
chaos42-nurse <gabriele.savone@gmail.com>
2026-09-23 18:55:45 +0200

    Create emergenza.js

---
+++ data/emergenza.js
@@ -0,0 +1,25 @@
+{
+  "id": "emergenza",
+  "title": "Elettrocardiogramma",
+  "icon": "🫀",
+  "description": "Archivio infermieristico dedicato all'elettrocardiogramma.",
+  "sections": [
+    {
+      "title": "Valori principali",
+      "items": [
+        "Frequenza cardiaca",
+        "Intervallo PR",
+        "Durata QRS",
+        "Intervallo QT/QTc"
+      ]
+    },
+    {
+      "title": "Argomenti",
+      "items": [
+        "Onde P, QRS e T",
+        "Ritmo sinusale",
+        "Alterazioni del ritmo"
+      ]
+    }
+  ]
+}
