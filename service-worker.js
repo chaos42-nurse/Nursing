9eda2671879efcae3b55ea9f76954b5acecf53b9
chaos42-nurse <gabriele.savone@gmail.com>
2026-09-23 13:32:40 +0200

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,6 +1,43 @@
-index.html
-style.css
-app.js
-data/
-assets/
-icone
+const CACHE_NAME = "nursing-nfc-v1";
+
+const FILES_TO_CACHE = [
+    "./",
+    "./index.html",
+    "./style.css",
+    "./app.js",
+    "./manifest.json",
+
+    "./data/ecg.json",
+    "./data/farmaci.json",
+    "./data/laboratorio.json",
+    "./data/emergenze.json"
+];
+
+self.addEventListener("install", event => {
+
+    event.waitUntil(
+        caches.open(CACHE_NAME)
+            .then(cache => cache.addAll(FILES_TO_CACHE))
+    );
+
+});
+
+
+self.addEventListener("fetch", event => {
+
+    event.respondWith(
+
+        caches.match(event.request)
+            .then(response => {
+
+                if (response) {
+                    return response;
+                }
+
+                return fetch(event.request);
+
+            })
+
+    );
+
+});
